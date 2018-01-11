49537dff62b47c48895458d5352f89004949e636
MarcBits <marcbits@outlook.com>
2018-01-11 13:12:48 -0500

    Initial commit.

--- SimpleToDo/SimpleToDoVC.swift
+++ SimpleToDo/SimpleToDoVC.swift
@@ -1,5 +1,5 @@
 //
-//  MainVC.swift
+//  SimpleToDoVC.swift
 //  SimpleToDo
 //
 //  Created by Marc Cruz on 1/11/18.
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class MainVC: UITableViewController {
+class SimpleToDoVC: UITableViewController {
+    
+    let itemArray = ["Complete workout", "Buy bananas", "Pick up package"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //MARK: - Tableview Datasource Methods
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SimpleToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK: - Tableview Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 
 }
 
