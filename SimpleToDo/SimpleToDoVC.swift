b4d921901ac48ec4dd9037fe50bee202f615054b
MarcBits <marcbits@outlook.com>
2018-01-15 14:06:20 -0500

    Data persisted via UserDefaults.

--- SimpleToDo/SimpleToDoVC.swift
+++ SimpleToDo/SimpleToDoVC.swift
@@ -11,10 +11,15 @@ import UIKit
 class SimpleToDoVC: UITableViewController {
     
     var itemArray = ["Complete workout", "Buy bananas", "Pick up package"]
+    
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
     }
     
     //MARK: - Tableview Datasource Methods
@@ -59,6 +64,8 @@ class SimpleToDoVC: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
             self.tableView.reloadData()
         }
