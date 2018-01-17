fa41beda97b0ab321d851e5204c27f65079e2db8
MarcBits <marcbits@outlook.com>
2018-01-17 12:41:29 -0500

    Convert to MVC design pattern.

--- SimpleToDo/Controller/SimpleToDoVC.swift
+++ SimpleToDo/Controller/SimpleToDoVC.swift
@@ -10,14 +10,17 @@ import UIKit
 
 class SimpleToDoVC: UITableViewController {
     
-    var itemArray = ["Complete workout", "Buy bananas", "Pick up package"]
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+        let newItem = Item(itemTitle: "Buy Groceries", itemCompleted: false)
+        itemArray.append(newItem)
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
             itemArray = items
         }
     }
@@ -35,8 +38,13 @@ class SimpleToDoVC: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "SimpleToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
         
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+
         return cell
     }
     
@@ -44,11 +52,9 @@ class SimpleToDoVC: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -63,7 +69,8 @@ class SimpleToDoVC: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
-            self.itemArray.append(textField.text!)
+            let newItem = Item(itemTitle: textField.text!, itemCompleted: false)
+            self.itemArray.append(newItem)
             
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
---
+++ SimpleToDo/Model/Item.swift
@@ -0,0 +1,19 @@
+//
+//  Item.swift
+//  SimpleToDo
+//
+//  Created by Marc Cruz on 1/17/18.
+//  Copyright © 2018 MarcBits. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    var title: String = ""
+    var done: Bool = false
+    
+    init (itemTitle: String, itemCompleted: Bool) {
+        self.title = itemTitle
+        self.done = itemCompleted
+    }
+}
